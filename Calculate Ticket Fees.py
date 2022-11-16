bc0f3bd248825aa1a920c60195568e815acd0df9
Mehedi Hasan <mehedihasan2882@icloud.com>
2022-11-16 14:33:17 +0600

    init

--- Calculate Ticket Fees.py
+++ Calculate Ticket Fees.py
@@ -1,15 +1,15 @@
 print("Welcome to the Roller Coaster")
 height=float(input("What's your height in feet? "))
-if height>=5 :
+if height >= 5:
     print("You are eligible to ride on the Roller Coaster!")
     age=int(input("How old are you? "))
     if age <= 12:
         print("You have to pay $5 to proceed further.")
-    elif age <18:
+    elif age < 18:
         print("You have to pay $10. ")
-    elif age <22:
+    elif age < 22:
         print("You have to pay $12")
     else:
         print("You have to pay adult fees, which is $16 in order to proceed further.")
 else:
-    print("You have to wait more to achive the required height.")
+    print("You have to wait more to achieve the required height.")
--- interecticve_BMI.py
+++ interecticve_BMI.py
@@ -1,1 +1,13 @@
-height = int(input("What is your height in metres? "))
+height = float(input("What is your height in metres? "))
+weight = int(input("What is your weight in Kilogram?"))
+bmi= round(weight/(height**2))
+if bmi<18.5:
+    print("You are underweight. You have to eat more healthy foods.")
+elif bmi<24:
+    print("Normal weight")
+elif bmi<30:
+    print("Overweight")
+elif bmi<35:
+    print("obsessed")
+else:
+    print("Clinically Obsessed")
