be37d9700e4cf01dea482ac656e9a211d1380976
Mehedi Hasan <mehedihasan2882@icloud.com>
2022-11-17 20:53:59 +0600

    commit to master

---
+++ Random.py
@@ -0,0 +1,7 @@
+import random
+random_integer= random.randint(0,5)
+# random.randint(x,y)-- here it's gonna create a number in between x to y....
+print(random_integer)
+
+random_float= random.random()*5
+print(random_float)
