6da3d82a11f4ac7d20f8c38be58718f81119e9d5
Mehedi Hasan <mehedihasan2882@icloud.com>
2022-11-19 23:18:37 +0600

    commit to master

---
+++ Who will pay bill.py
@@ -0,0 +1,20 @@
+# import random
+# #here goes a program to
+# seed_number=int(input("Enter a seed number"))
+# random.seed(seed_number)
+#
+# random_person=int(input("Who want's to pay the bill? Use \"(,)\" and a space."))
+# split_random=  random_person.split(", ")
+# print(split_random)
+#
+#
+#
+#
+import random
+seed_num=int(input("Seed number: \n"))
+random.seed(seed_num)
+randomNum= random.randint(1,5)
+print(randomNum)
+individual_arrey=["First", "Second"]
+
+
